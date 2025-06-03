f54b8d49e2950b34694d7f0831e8f444a0616065
Johannes U. Lange <julange.astro@pm.me>
2025-06-03 15:54:50 -0400

    new release

--- tabcorr/__init__.py
+++ tabcorr/__init__.py
@@ -5,5 +5,5 @@ from .interpolator import Interpolator
 from . import corrfunc
 from . import database
 
-__version__ = '1.1.1'
+__version__ = '1.2.0'
 __all__ = ["TabCorr", "Interpolator", "corrfunc", "database"]
